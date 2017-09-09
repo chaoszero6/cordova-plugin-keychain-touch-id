66c1b20a82fc6bd74b9f0db50bf002cce6c89ef7
chaoszero6 <chaoszero6@gmail.com>
2017-09-09 15:03:09 +0530

    Dialog Title and Custom message
    adding customised message for Dialog Title and Custom message while save of password

--- www/touchid.js
+++ www/touchid.js
@@ -5,11 +5,11 @@ var touchid = {
 	isAvailable: function(successCallback, errorCallback){
 		exec(successCallback, errorCallback, "TouchID", "isAvailable", []);
 	},
-	save: function(key,password, successCallback, errorCallback) {
-		exec(successCallback, errorCallback, "TouchID", "save", [key,password]);
+	save: function(key,password, successCallback, errorCallback, message, dialogTitle) {
+		exec(successCallback, errorCallback, "TouchID", "save", [key,password,message,dialogTitle]);
 	},
-	verify: function(key,message,successCallback, errorCallback){
-		exec(successCallback, errorCallback, "TouchID", "verify", [key,message]);
+	verify: function(key,message,successCallback, errorCallback, dialogTitle){
+		exec(successCallback, errorCallback, "TouchID", "verify", [key,message,dialogTitle]);
 	},
 	has: function(key,successCallback, errorCallback){
 		exec(successCallback, errorCallback, "TouchID", "has", [key]);
